adee5975dc94a0846777d9640e26da77dc265224
Hae-gun <newchk610@gmail.com>
2024-04-17 23:55:09 +0900

    feature: make email domain

--- something-back/src/main/java/com/example/somethingback/domain/user/Email.java
+++ something-back/src/main/java/com/example/somethingback/domain/user/Email.java
@@ -1,9 +1,14 @@
 package com.example.somethingback.domain.user;
 
 import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.Embeddable;
 
+@Embeddable
 public class Email {
     private String email;
+
+    public Email() {
+    }
 
     private Email(String email) {
         if(validation(email)){
@@ -14,6 +19,10 @@ public class Email {
 
     public static Email of(String email){
         return new Email(email);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getEmail() {
